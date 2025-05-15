7333f2aea8136af3fc71438cf8787b8b24adbbca
Saiful0044 <saifulislam003244@gail.com>
2025-05-15 08:56:12 +0600

    Initial commit with cooriecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project implements a hybrid recommender system for music tracks on Spotify, combining content-based and collaborative filtering approaches to provide personalized song recommendations.',
+    author='Saiful Islam',
+    license='',
+)
